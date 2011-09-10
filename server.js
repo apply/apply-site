67c8dc4f4bacea2e282b21cd416a6285fe6b92cd
Ian <jorgensen.ian@gmail.com>
2011-09-10 14:49:40 -0700

    routes finished

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var db = require('mongojs').connect('mongodb://root:root@staff.mongohq.com:10041
 var noop = function() {};
 
 server.get('/css/*.css', bark.stylus('./static/style/{*}.styl'));
+server.get('/js/*', bark.rex('./static/js/*'));
 
 var echo = function(request, response) {
 	response.writeHead(200);
@@ -13,17 +14,36 @@ var echo = function(request, response) {
 
 server.get('/', echo);
 
-server.get('/blob/create', echo); // create view
-server.get('/blob/{id}/create', echo); // create item
+// create blob
+server.get('/blobs/create', echo);
 
-server.get('/blob/{id}/view', echo); // select view
+// select view
+server.get('/blobs/{id}/view', echo);
 
-server.get('/blob/{id}/grid', echo);
-server.get('/blob/{id}/tabular', echo);
-server.get('/blob/{id}/map', echo);
-server.get('/blob/{id}/gallery', echo);
+// blob view
+server.get('/blobs/{id}', echo);
+server.get('/blobs/{blob}/grid', echo);
+server.get('/blobs/{blob}/tabular', echo);
+server.get('/blobs/{blob}/map', echo);
+server.get('/blobs/{blob}/gallery', echo);
 
-server.get('/item/{id}', echo);
-server.get('/item/{id}/edit', echo);
+// items
+server.get('/blobs/{blob}/items/create', echo);
+server.get('/items/{item}', echo);
+server.get('/items/{item}/edit', echo);
+
+// api
+
+// blob create
+server.post('/api/blobs', echo);
+server.post('/api/blobs/{blob}/view', echo);
+
+// blob view
+server.get('/api/blobs/{blob}', echo);
+server.get('/api/blobs/{blob}/items', echo);
+
+// items
+server.post('/api/blobs/{blob}/items', echo);
+server.get('/api/blobs/{blob}/items/{item}', echo);
 
 server.listen(8888);
